727db74a70cbffa5e44b2bfbcfe0acafa4cd8739
NinjaDev108 <jsshankar@gmail.com>
2025-11-06 14:49:56 -0800

    Consolidate launch files.

--- basic examples/ocs2_mobile_manipulator_ros/launch/include/visualize.launch.py
+++ basic examples/ocs2_mobile_manipulator_ros/launch/include/visualize.launch.py
@@ -10,32 +10,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
-            name='urdfFile',
-            # default_value=get_package_share_directory('ocs2_robotic_assets') + "/resources/mobile_manipulator/franka/urdf/panda.urdf"
-        ),
-        launch.actions.DeclareLaunchArgument(
-            name='test',
-            default_value='false'
-        ),
-        launch.actions.DeclareLaunchArgument(
             name='rviz',
             default_value='true'
         ),
         launch.actions.DeclareLaunchArgument(
             name='rvizconfig',
             default_value=get_package_share_directory('ocs2_mobile_manipulator_ros') + "/rviz/mobile_manipulator.rviz"
-        ),
-        Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            output="screen",
-            arguments=[LaunchConfiguration("urdfFile")],
-        ),
-        Node(
-            package="joint_state_publisher_gui",
-            executable="joint_state_publisher_gui",
-            arguments=[LaunchConfiguration("urdfFile")],
-            condition=IfCondition(LaunchConfiguration("test")),
         ),
         Node(
             package='rviz2',
--- basic examples/ocs2_mobile_manipulator_ros/launch/manipulator_viper.launch.py
+++ basic examples/ocs2_mobile_manipulator_ros/launch/manipulator_viper.launch.py
@@ -1,58 +1,188 @@
 import os
-import sys
-
-import launch
-import launch_ros.actions
+import re
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition, UnlessCondition
 from ament_index_python.packages import get_package_share_directory
+
+def is_wsl():
+    try:
+        with open('/proc/version', 'r') as f:
+            version_info = f.read().lower()
+            return 'microsoft' in version_info or 'wsl' in version_info
+    except FileNotFoundError:
+        return False
 
 
 def generate_launch_description():
-    ld = launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
+    prefix = "gnome-terminal --"
+    if is_wsl():
+        prefix = "xterm -e"
+        print("Current system is WSL, use xterm as terminal")
+    else:
+        print("Current system is not WSL, use gnome-terminal as terminal")
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
             name='rviz',
-            default_value='true'
-        ),
-        launch.actions.DeclareLaunchArgument(
-            name='debug',
             default_value='false'
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
+            name='target_marker',
+            default_value='true',
+            description='Whether to enable target marker'
+        ),
+        DeclareLaunchArgument(
             name='dummy',
             default_value='true',
             description='Whether to enable dummy mrt node'
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='urdfFile',
             default_value=get_package_share_directory(
                 'ocs2_robotic_assets') + '/resources/mobile_manipulator/viper/urdf/vx300s.urdf'
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='taskFile',
             default_value=get_package_share_directory(
                 'ocs2_mobile_manipulator') + '/config/viper/task.info'
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='libFolder',
             default_value=get_package_share_directory(
                 'ocs2_mobile_manipulator') + '/auto_generated/viper'
         ),
-        launch.actions.IncludeLaunchDescription(
-            launch.launch_description_sources.PythonLaunchDescriptionSource(
+        DeclareLaunchArgument(
+            name='debug',
+            default_value='false'
+        ),
+        DeclareLaunchArgument(
+            name='enableJoystick',
+            default_value='false',
+            description='Whether to enable joystick control'
+        ),
+        DeclareLaunchArgument(
+            name='enableAutoPosition',
+            default_value='false',
+            description='Whether to enable automatic marker position updates'
+        ),
+        Node(
+            package='ocs2_mobile_manipulator_ros',
+            executable='mobile_manipulator_mpc',
+            name='mobile_manipulator_mpc',
+            prefix=prefix,
+            condition=IfCondition(LaunchConfiguration("debug")),
+            output='screen',
+            parameters=[
+                {
+                    'taskFile': LaunchConfiguration('taskFile')
+                },
+                {
+                    'urdfFile': LaunchConfiguration('urdfFile')
+                },
+                {
+                    'libFolder': LaunchConfiguration('libFolder')
+                }
+            ]
+        ),
+        Node(
+            package='ocs2_mobile_manipulator_ros',
+            executable='mobile_manipulator_mpc_node',
+            name='mobile_manipulator_mpc',
+            condition=UnlessCondition(LaunchConfiguration("debug")),
+            output='screen',
+            parameters=[
+                {
+                    'taskFile': LaunchConfiguration('taskFile')
+                },
+                {
+                    'urdfFile': LaunchConfiguration('urdfFile')
+                },
+                {
+                    'libFolder': LaunchConfiguration('libFolder')
+                }
+            ]
+        ),
+        Node(
+            package='ocs2_mobile_manipulator_ros',
+            executable='mobile_manipulator_dummy_mrt_node',
+            name='mobile_manipulator_dummy_mrt_node',
+            prefix=prefix,
+            output='screen',
+            condition=IfCondition(LaunchConfiguration("dummy")),
+            parameters=[
+                {
+                    'taskFile': LaunchConfiguration('taskFile')
+                },
+                {
+                    'urdfFile': LaunchConfiguration('urdfFile')
+                },
+                {
+                    'libFolder': LaunchConfiguration('libFolder')
+                }
+            ]
+        ),
+        Node(
+            package='ocs2_mobile_manipulator_ros',
+            executable='mobile_manipulator_mrt_node',
+            name='mobile_manipulator_mrt_node',
+            prefix=prefix,
+            output='screen',
+            condition=UnlessCondition(LaunchConfiguration("dummy")),
+            parameters=[
+                {
+                    'taskFile': LaunchConfiguration('taskFile')
+                },
+                {
+                    'urdfFile': LaunchConfiguration('urdfFile')
+                },
+                {
+                    'libFolder': LaunchConfiguration('libFolder')
+                },
+                {
+                    'robotStateTopic': '/vx300s/arm_controller/controller_state'
+                },
+                {
+                    'robotCommandTopic': '/vx300s/commands/joint_group'
+                }
+            ]
+        ),
+        Node(
+            package='ocs2_mobile_manipulator_ros',
+            executable='mobile_manipulator_target',
+            name='mobile_manipulator_target',
+            condition=IfCondition(LaunchConfiguration("target_marker")),
+            parameters=[
+                {
+                    'taskFile': LaunchConfiguration('taskFile')
+                },
+                {
+                    'enableJoystick': LaunchConfiguration('enableJoystick')
+                },
+                {
+                    'enableAutoPosition': LaunchConfiguration('enableAutoPosition')
+                },
+            ],
+            output='screen',
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
                 os.path.join(get_package_share_directory(
-                    'ocs2_mobile_manipulator_ros'), 'launch/include/mobile_manipulator_viper.launch.py')
+                    'ocs2_mobile_manipulator_ros'), 'launch/include/visualize.launch.py')
             ),
             launch_arguments={
-                'rviz': launch.substitutions.LaunchConfiguration('rviz'),
-                'debug': launch.substitutions.LaunchConfiguration('debug'),
-                'urdfFile': launch.substitutions.LaunchConfiguration('urdfFile'),
-                'dummy': launch.substitutions.LaunchConfiguration('dummy'),
-                'taskFile': launch.substitutions.LaunchConfiguration('taskFile'),
-                'libFolder': launch.substitutions.LaunchConfiguration('libFolder')
-            }.items()
+                'rviz': LaunchConfiguration('rviz')
+            }.items(),
+            condition=IfCondition(LaunchConfiguration("rviz"))
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="screen",
+            condition=IfCondition(LaunchConfiguration("dummy")),
+            arguments=[LaunchConfiguration("urdfFile")],
         )
     ])
-    return ld
-
-
-if __name__ == '__main__':
-    generate_launch_description()
